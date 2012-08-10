f3f477e839d0012acb2f821b44999313bd1d287d
Matt <antidragon.is@googlemail.com>
2012-08-10 10:14:58 +0100

    Application created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlRefinery::Application.config.secret_token = '78704c8f0f547c24d87a981d2d31385f32ca0c5c810165df3d838fc4f4307469cfe9d8f50f38c047f3df230c59b98708578b2e38ff005489f2f10452b9bda2b3'
